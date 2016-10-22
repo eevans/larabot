681a95448c10c654ee8dfb67b665878069e5b96f
Eric Evans <eevans@sym-link.com>
2016-10-22 17:32:02 -0500

    added control of pan-tilt

---
+++ larabot.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+
+import serial
+import sys
+
+
+class PanTilt(object):
+    term = '\r'
+    limit = 180    # 180 degree limit
+
+    def __init__(self, port):
+        self.serial = serial.Serial(port)
+        self.current_pan = self.current_tilt = 0
+        self.tilt(140)
+        self.pan(90)
+
+    def tilt(self, pos):
+        if pos < 0 or pos > PanTilt.limit:
+            return
+        self.command(0, pos)
+        self.current_tilt = pos
+
+    def pan(self, pos):
+        if pos < 0 or pos > PanTilt.limit:
+            return
+        self.command(1, pos)
+        self.current_pan = pos
+
+    def command(self, axis, position):
+        return self.serial.write("{},{}{}".format(axis, position, PanTilt.term))
+
+
+def main():
+    controller = PanTilt("/dev/ttyACM0")
+    controller.pan(int(sys.argv[1]))
+    controller.tilt(int(sys.argv[2]))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print >>sys.stderr, "Usage: {} <pan> <tilt>".format(sys.argv[0])
+        sys.exit(1)
+    main()
